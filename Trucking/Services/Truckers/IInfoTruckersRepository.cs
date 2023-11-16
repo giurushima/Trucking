7fa6bf7ad0a74ab5a627fcaf4b5239d7e747b922
giurushima <giurushima@hotmail.com>
2023-11-16 20:25:43 -0300

    Carpetas separadas para repositorios services de Truckers y trips

---
+++ Trucking/Services/Truckers/IInfoTruckersRepository.cs
@@ -0,0 +1,16 @@
+﻿using Trucking.Entities;
+using Trucking.Models.Create;
+using Trucking.Models.Update;
+
+namespace Trucking.Services
+{
+    public interface IInfoTruckersRepository
+    {
+        public IEnumerable<Trucker> GetTruckers();
+        public Trucker? GetTrucker(int id);
+        public void CreateTrucker(CreateTruckerDto trucker);
+        public void UpdateTrucker(int id, UpdateTruckerDto trucker);
+        public void DeleteTrucker(int id);
+        public void SaveChanges();
+    }
+}
