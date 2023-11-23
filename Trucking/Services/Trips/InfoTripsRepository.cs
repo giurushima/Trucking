960e277b3f62ed782d5254830276436d3a19bff6
giurushima <giurushima@hotmail.com>
2023-11-23 18:09:05 -0300

    Cambios en interfaz y clase de trips para services

--- Trucking/Services/Trips/InfoTripsRepository.cs
+++ Trucking/Services/Trips/InfoTripsRepository.cs
@@ -1,6 +1,29 @@
-﻿namespace Trucking.Services.Trips
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Trucking.Context;
+using Trucking.Entities;
+using Trucking.Models.General;
+
+namespace Trucking.Services.Trips
 {
-    public class InfoTripsRepository
+    public class InfoTripsRepository : IInfoTripsRepository
     {
+        private readonly TruckContext _context;
+
+        public InfoTripsRepository(TruckContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Trip> GetTrips(int idTrucker);
+        {
+            return _context.Trips.Where(x => x.TruckerId == idTrucker).ToList();
+        }
+
+        public Trip GetTrip(int idTrucker, int idTrip) 
+        {
+            return _context.Trips.Where(x => x.TruckerId == idTrucker && x.Id == idTrip).FirstOrDefault();
+        }
+
     }
 }
