b680ffeba566053c9230f346fecb45ab3161c9c3
giurushima <giurushima@hotmail.com>
2023-11-23 22:41:36 -0300

    Agregado de datos para entidad de users

--- Trucking/Entities/User.cs
+++ Trucking/Entities/User.cs
@@ -1,6 +1,18 @@
-﻿namespace Trucking.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Trucking.Entities
 {
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [MaxLength(32)]
+        public string? Name { get; set; }
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        public string? Password { get; set; }
     }
 }
