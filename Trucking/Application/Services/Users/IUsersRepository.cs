dcacb7f3d36c2e5bd1aa8103bb6a3c4cca5142ce
Gabriel <giurushima@hotmail.com>
2024-09-17 19:30:40 -0300

    CHANGE: Reemplazo en directorios.

---
+++ Trucking/Application/Services/Users/IUsersRepository.cs
@@ -0,0 +1,11 @@
+﻿using Trucking.Entities;
+using Trucking.Models.Auth;
+
+namespace Trucking.Services.Users
+{
+    public interface IUsersRepository
+    {
+        public User? ValidateUser(AuthenticationRequestBody authRequestBody);
+        public bool SaveChange();
+    }
+}
