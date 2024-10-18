6daa08e18c3aeb792a62a976a95a230e60335e69
Gabriel <giurushima@hotmail.com>
2024-10-17 22:28:21 -0300

    CHANGE: Variables definidas para Dto User.

--- Trucking/Application/Models/Create/CreateUserDto.cs
+++ Trucking/Application/Models/Create/CreateUserDto.cs
@@ -1,6 +1,20 @@
-﻿namespace Trucking.Application.Models.Create
+﻿using System.ComponentModel.DataAnnotations;
+using Trucking.Enums;
+
+namespace Trucking.Application.Models.Create
 {
     public class CreateUserDto
     {
+        [Required(ErrorMessage = "Ingrese nombre (menor a 32 caracteres)")]
+        [MaxLength(32)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Ingrese nombre de usuario (menor a 15 caracteres)")]
+        [MaxLength(15)]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Ingrese contraseña (mayor a 4 caracteres)")]
+        [MinLength(4)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Ingrese rol")]
+        public Roles Roles { get; set; }
     }
 }
