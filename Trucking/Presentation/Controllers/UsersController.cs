195eba7245e46766934d71fbaba7a1f6d068b910
Gabriel <giurushima@hotmail.com>
2024-10-17 22:31:57 -0300

    CHANGE: Agregado de endpoint para GetUsers y definicion de interfaz (completar).

--- Trucking/Presentation/Controllers/UsersController.cs
+++ Trucking/Presentation/Controllers/UsersController.cs
@@ -1,6 +1,32 @@
-﻿namespace Trucking.Presentation.Controllers
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Trucking.Application.Models.General;
+using Trucking.Models.General;
+using Trucking.Services.Users;
+
+namespace Trucking.Presentation.Controllers
 {
-    public class UsersController
+    [ApiController]
+    [Route("api/users")]
+    [Authorize]
+    public class UsersController : Controller
     {
-    }
+        private readonly IInfoUsersRepository _infoUsersRepository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IInfoUsersRepository IInfoUsersRepository, IMapper mapper)
+        {
+            _infoUsersRepository = IInfoUsersRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public JsonResult GetUsers()
+        {
+            var users = _infoUsersRepository.GetUsers();
+
+            return new JsonResult(_mapper.Map<ICollection<UserDto>>(users));
+        }
+    } 
 }
