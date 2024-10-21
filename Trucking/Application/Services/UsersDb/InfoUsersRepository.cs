2446c1901fa374347078d30a220af4d4f19c74ed
Gabriel <giurushima@hotmail.com>
2024-10-21 19:14:17 -0300

    ADD: Agregado de archivo para services de users, agregado y definicion para tomar datos de db context y retornar get de usuarios generales del sistema.

---
+++ Trucking/Application/Services/UsersDb/InfoUsersRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trucking.Context;
+using Trucking.Entities;
+
+namespace Trucking.Application.Services.UsersDb
+{
+    public class InfoUsersRepository : IInfoUsersRepository
+    {
+        private readonly TruckContext _context;
+
+        public InfoUsersRepository(TruckContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<User> GetUsers()
+        {
+            return _context.Users;
+        }
+    }
+}
