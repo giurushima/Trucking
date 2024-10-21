be76a6b03c2028c6a0db8428f939612ab5e0a6f5
Gabriel <giurushima@hotmail.com>
2024-10-21 19:17:31 -0300

    ADD: Agregado de mapper para user para automatizacion de conversion entre tipos de objetos.

---
+++ Trucking/AutoMapperProfiles/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Trucking.Entities;
+using Trucking.Application.Models.Create;
+using Trucking.Application.Models.General;
+using Trucking.Application.Models.Update;
+
+namespace Trucking.AutoMapperProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile() {
+            CreateMap<Entities.User, UserDto>();
+            CreateMap<CreateUserDto, Entities.User>();
+            CreateMap<UpdateUserDto, Entities.User>();
+        }
+    }
+}
