b12d38f56f993da3b39647b426d8d6338a3c5803
Gabriel <giurushima@hotmail.com>
2024-11-07 23:42:28 -0300

    CHANGE: Cambios para servicio User en get user, create user y save changes.

--- Trucking/Application/Services/UsersDb/InfoUsersRepository.cs
+++ Trucking/Application/Services/UsersDb/InfoUsersRepository.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Trucking.Application.Models.Create;
+using Trucking.Application.Models.Update;
 using Trucking.Context;
 using Trucking.Entities;
+using Trucking.Models.Create;
 
 namespace Trucking.Application.Services.UsersDb
 {
     public class InfoUsersRepository : IInfoUsersRepository
     {
-        private readonly TruckContext _context;
+        internal readonly TruckContext _context;
 
         public InfoUsersRepository(TruckContext context)
         {
@@ -15,6 +18,42 @@ namespace Trucking.Application.Services.UsersDb
         public IEnumerable<User> GetUsers()
         {
             return _context.Users;
+        }
+
+        public User GetUser(int idUser)
+        {
+            return _context.Users.Where(x => x.Id == idUser).FirstOrDefault();
+        }
+
+        public void CreateUser(CreateUserDto user)
+        {
+            var newUser = new User
+            {
+                Name = user.Name,
+                UserName = user.UserName,
+                Password = user.Password,
+            };
+            _context.Users.Add(newUser);
+            SaveChanges();
+        }
+
+        public void UpdateUser(int id, UpdateUserDto user)
+        {
+            var existingUser = _context.Users.FirstOrDefault(x => x.Id == id);
+
+            if (existingUser != null)
+            {
+                existingUser.Name = user.Name;
+                existingUser.UserName = user.UserName;
+                existingUser.Password = user.Password;
+
+                SaveChanges();
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
         }
     }
 }
