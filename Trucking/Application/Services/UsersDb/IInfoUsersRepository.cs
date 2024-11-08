7285ea4b0a464599af423931055878941ccf84b4
Gabriel <giurushima@hotmail.com>
2024-11-07 23:29:15 -0300

    CHANGE: Agregado para publics de Create, Update, Get y Savechanges.

--- Trucking/Application/Services/UsersDb/IInfoUsersRepository.cs
+++ Trucking/Application/Services/UsersDb/IInfoUsersRepository.cs
@@ -1,9 +1,16 @@
-﻿using Trucking.Entities;
+﻿using Trucking.Application.Models.Create;
+using Trucking.Application.Models.Update;
+using Trucking.Entities;
+using Trucking.Models.Update;
 
 namespace Trucking.Application.Services.UsersDb
 {
     public interface IInfoUsersRepository
     {
-        public IEnumerable<User> GetUsers();    
+        public IEnumerable<User> GetUsers();
+        public User? GetUser(int idUser);
+        public void CreateUser(CreateUserDto user);
+        public void UpdateUser(int id, UpdateUserDto user);
+        public void SaveChanges();
     }
 }
