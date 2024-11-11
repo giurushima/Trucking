0965528883278b8624bde945680b8f745a6dc3f2
Gabriel <giurushima@hotmail.com>
2024-11-11 20:18:37 -0300

    CHANGE: Agregado de using para modelos y entidad. Cambio en variable para solicitud por id de usuario. Agregado de endpoint para creacion de usuario post, endpoint put para actualizar usuario por id y endpoint delete para eliminar usuario por id.

--- Trucking/Presentation/Controllers/UsersController.cs
+++ Trucking/Presentation/Controllers/UsersController.cs
@@ -1,9 +1,14 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Trucking.Application.Models.Create;
 using Trucking.Application.Models.General;
+using Trucking.Application.Models.Update;
 using Trucking.Application.Services.UsersDb;
+using Trucking.Entities;
 using Trucking.Models.General;
+using Trucking.Models.Update;
 using Trucking.Services.Users;
 
 namespace Trucking.Presentation.Controllers
@@ -30,7 +35,7 @@ namespace Trucking.Presentation.Controllers
             return new JsonResult(_mapper.Map<ICollection<UserDto>>(users));
         }
 
-        [HttpGet("{idTrip}")]
+        [HttpGet("{idUser}")]
         public ActionResult GetUser(int idUser)
         {
             Entities.User? user = _infoUsersRepository.GetUser(idUser);
@@ -41,6 +46,42 @@ namespace Trucking.Presentation.Controllers
 
             return Ok(_mapper.Map<UserDto>(user));
         }
+
+        [HttpPost(Name = "GetUsers")]
+        public ActionResult<UserDto> CreateUser(CreateUserDto userDto)
+        {
+            var user = _mapper.Map<User>(userDto);
+
+            _infoUsersRepository.CreateUser(userDto);
+
+            return CreatedAtRoute(
+                "GetUsers",
+                new
+                {
+                    Id = user.Id,
+                },
+                _mapper.Map<UserDto>(user));
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateUser(int id, UpdateUserDto userDto)
+        {
+            var existingUser = _infoUsersRepository.GetUser(id);
+            if (existingUser == null)
+                return NotFound();
+
+            _infoUsersRepository.UpdateUser(id, userDto);
+
+            _mapper.Map(userDto, existingUser);
+
+            _infoUsersRepository.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteUser(int id)
+        {
             var userToDelete = _infoUsersRepository.GetUser(id);
             if (userToDelete == null)
             {
@@ -53,5 +94,5 @@ namespace Trucking.Presentation.Controllers
 
             return NoContent();
         }
-    } 
+    }
 }
